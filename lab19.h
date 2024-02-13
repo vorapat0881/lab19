ca516f8e37ea17fbf71e14838c350dfae3512975
vorapat0881 <vorapat_g@cmu.ac.th>
2024-02-13 16:01:06 +0700

    lab19_1

--- lab19.h
+++ lab19.h
@@ -63,13 +63,38 @@ void Unit::newTurn(){
 	guard_on = false;
 }
 
+bool Unit::isDead(){
+    if(hp <= 0)return true;
+    else return false;
+    }
 
+void Unit::guard(){
+    guard_on = true;
+    }
 
-/////////////////////////////////////////////////////////////////////////////////////
-//Write function members isDead(), guard(), heal(), beAttacked(), and attack() here//
-/////////////////////////////////////////////////////////////////////////////////////
+int Unit::beAttacked(int oppatk){
+    int dmg = oppatk - def;
+    if(guard_on == true){ 
+    dmg = dmg/3;
+    }
+    hp = hp - dmg;
 
+    return dmg;
+}
 
+int Unit::attack(Unit &at){
+	return at.beAttacked(atk);
+}
+
+int Unit::heal(){
+    int heal = rand()%21+10;
+    if(hp+heal > hpmax){
+    heal = hpmax - hp;
+    hp = hpmax;
+    }
+    else hp = hp + heal;
+    return heal;
+    }
 
 void drawScene(char p_action,int p,char m_action,int m){
 	cout << "                                                       \n";
